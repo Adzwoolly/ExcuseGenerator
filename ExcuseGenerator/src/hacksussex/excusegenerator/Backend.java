f55c5f2210d83ca70539d69b7f758f5792802f0c
Adam Woollen <adamwoollen@gmail.com>
2016-04-23 16:31:15 +0100

    Prints least common word
    Loads list of words and their rankings.
    Takes input and determines which word is least common.
    Prints the least common word.

---
+++ ExcuseGenerator/src/hacksussex/excusegenerator/Backend.java
@@ -0,0 +1,66 @@
+package hacksussex.excusegenerator;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Backend {
+	
+	HashMap<String, Integer> common = new HashMap<String, Integer>();
+	
+	String problem = "";
+	
+	public Backend(){
+		String wordRankings = "WordRankings.csv";
+		
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(wordRankings));
+			String line;
+			while((line = reader.readLine()) != null){
+				String[] values = line.split(",");
+				common.put(values[0], Integer.parseInt(values[1]));
+			}
+			
+			reader.close();
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Generates an excuse
+	 * @param problem The user inputted situation for which an excuse is needed
+	 * @return An excuse
+	 */
+	public String getExcuse(String problem){
+		this.problem = problem;
+		
+		String[] words = problem.split(" ");
+		//Higher number is less common
+		int leastCommonRanking = 0;
+		String leastCommonWord = "";
+		
+		for(String word : words){
+			int ranking = common.get(word);
+			if(ranking > leastCommonRanking){
+				leastCommonRanking = ranking;
+				leastCommonWord = word;
+			}
+		}
+		
+		System.out.println("Least common word: " + leastCommonWord);
+		
+		return null;
+	}
+
+}
