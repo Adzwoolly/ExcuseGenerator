b14985efed7674a69704f889d3ed6ff49a0d1c2d
Adam Woollen <adamwoollen@gmail.com>
2016-04-24 00:07:46 +0100

    Commented code and did a little refactoring

--- ExcuseGenerator/src/hacksussex/excusegenerator/Backend.java
+++ ExcuseGenerator/src/hacksussex/excusegenerator/Backend.java
@@ -18,16 +18,20 @@ public class Backend {
 	private HashMap<String, Integer> wordFrequencyRankings;
 	
 	public Backend(){
-		String wordRankings = "WordRankings.csv";
-		randomExcuses = new String[]{"I would but, my cat ate my list of excuses, choked, and died.",
-				"As much as I love to, the reason I can't do that isn't you - it's me.",
-				"It'd be great to do that - but I'm reading this really interesting blog post about how I keep making excuses for everything."};
 		wordFrequencyRankings = new HashMap<String, Integer>();
+		
+		//The file containing words and their frequencies
+		String wordRankings = "WordRankings.csv";
+		
 		try {
+			//Read the word frequencies file line by line
 			BufferedReader reader = new BufferedReader(new FileReader(wordRankings));
 			String line;
 			while((line = reader.readLine()) != null){
+				//It's a comma separated file, so split by comma to
+				//Separate the word and it's frequency rank.
 				String[] values = line.split(",");
+				//Insert the values into a HashMap to be used later
 				wordFrequencyRankings.put(values[0], Integer.parseInt(values[1]));
 			}
 			
@@ -41,12 +45,11 @@ public class Backend {
 			System.out.println("Euurghh!  Did you break the word ranking file?");
 		}
 		
+		//Initialise the array of random excuses with random excuses
 		randomExcuses = new String[]{"I would but, my cat ate my list of excuses, choked, and died.",
 				"As much as I love to, the reason I can't do that isn't you - it's me.",
 				"It'd be great to do that - but I'm reading this really interesting blog post about how I keep making excuses for everything."};
-		for(int i = 0; i < 10; i++){
-			System.out.println(randomExcuse());
-		}
+		
 	}
 	
 	
@@ -56,51 +59,64 @@ public class Backend {
 	 * @return An excuse
 	 */
 	public String getExcuse(String problem){
+		//Split the given sentence into an array of words
 		String[] words = problem.split(" ");
 		//Higher number is less common
 		int leastCommonRanking = 0;
 		String leastCommonWord = null;
 		String response = null;
 		
+		//Loop through all words inputted by user, determining which is least common
 		for(String word : words){
+			//format the word to be compatible with the HashMap
 			String formattedWord = formatWord(word);
 			Integer ranking = wordFrequencyRankings.get(formattedWord);
 			if(ranking != null){
+				//If this word is less common than the least common so far
 				if(ranking > leastCommonRanking){
+					//Set it to be the new least common
 					leastCommonRanking = ranking;
 					leastCommonWord = formattedWord;
 				}
 			}
 		}
 		
+		//If none of the user defined words were in the rankings list, use a random excuse
 		if(leastCommonWord == null){
 			response = randomExcuse();
 		}
 		
-		System.out.println("Response " + response);
+		System.out.println("Response: " + response);
 		
 		return response;
 	}
 	
 	/**
 	 * Formats the given string so it is suitable to search in the hashmap.
-	 * Converts string to lowercase and removes punctuation.
+	 * Converts string to lower case and removes punctuation.
 	 * @param word The word/string you wish to format
 	 * @return The formatted word/string, suitable for searching the hashmap
 	 */
 	private String formatWord(String word){
+		//Remove all symbols
 		word = word.replaceAll("[!\"#$%&'()*+,\\-./:;<=>?@[\\\\]^_`{|}~]", "");
 		word = word.toLowerCase();
 		return word;
 	}
-		
+	
+	/**
+	 * Generates a random excuse without consideration of an inputted string
+	 * @return
+	 */
 	private static String randomExcuse(){
+		String randomExcuse = randomExcuses[randomExcuseCount];
+		//Increment the counter to the next value until the maximum is reached, and then reset to 0.
 		if(randomExcuseCount < randomExcuses.length - 1){
 			randomExcuseCount++;
 		} else{
 			randomExcuseCount = 0;
 		}
-		return randomExcuses[randomExcuseCount];
+		return randomExcuse;
 	}
 
 }
